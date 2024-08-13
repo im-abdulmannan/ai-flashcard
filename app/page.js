3df4940f37d4f05862a86765d83fdcf2aeb3affe
Abdul Mannan <abdulmann1234@gmail.com>
2024-08-13 22:55:52 +0500

    Init

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
+'use client'
 import { Box, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const gotoHome = () => {
+    router.push("/home");
+  }
+  
+  useEffect(() => {
+    setTimeout(() => {
+      gotoHome();
+    }, 2000);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <Box>
       <Typography variant="h1">Welcome to Gemini AI Flashcard</Typography>
